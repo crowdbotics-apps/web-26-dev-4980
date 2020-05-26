cafa55fa0109a74c70ae5f9071f50d9c33990c13
Crowdbotics <team@crowdbotics.com>
2020-05-26 08:12:40 +0000

    Update admin title

--- web_26_dev_4980/urls.py
+++ web_26_dev_4980/urls.py
@@ -33,9 +33,9 @@ urlpatterns = [
     path("home/", include("home.urls")),
 ]
 
-admin.site.site_header = "web 26"
-admin.site.site_title = "web 26 Admin Portal"
-admin.site.index_title = "web 26 Admin"
+admin.site.site_header = "web 260"
+admin.site.site_title = "web 260 Admin Portal"
+admin.site.index_title = "web 260 Admin"
 
 # swagger
 schema_view = get_schema_view(
